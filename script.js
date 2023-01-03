b70a02cfe65cee947673a79e5d5381efda9d896e
dejourford <107910336+dejourford@users.noreply.github.com>
2023-01-02 18:17:14 -0600

    fixed issue of addition function adding strings

--- script.js
+++ script.js
@@ -18,8 +18,9 @@ let equals = document.querySelector('.equals')
 
 // create event listener for number clicks
 numbers.forEach((number) => number.addEventListener('click', function(e){
-    console.log(e.target.textContent)
+    // console.log(e.target.textContent)
     numberPressed = e.target.textContent
+    numberToInteger = Math.floor(numberPressed)
     useNumber()
 }))
 
@@ -30,7 +31,7 @@ function useNumber() {
     }
     
     if (currentScreen.textContent.length < 13){
-        currentScreen.textContent += numberPressed 
+        currentScreen.textContent += numberToInteger
     }
 }
 
@@ -57,27 +58,95 @@ decimalBtn.addEventListener('click', function(e){
 // define operations
 function divide(previousValue, currentValue) {
     solution = (previousValue / currentValue)
+    currentScreen.textContent = solution
+    
 }
 
 function multiply(previousValue, currentValue) {
     solution = (previousValue * currentValue)
+    currentScreen.textContent = solution
 }
 
 function subtract(previousValue, currentValue) {
     solution = (previousValue - currentValue)
+    currentScreen.textContent = solution
 }
 
 function add(previousValue, currentValue) {
     solution = (previousValue + currentValue)
+    currentScreen.textContent = solution
 }
 
 // create event listener for operation button clicks
 operators.forEach((operator) => operator.addEventListener('click', function(e){
-    console.log('clicked!')
-    console.log(`${e.target.textContent} has been clicked`)
-    // e.target.textContent = operator
+    if (previousScreen != 0) {
+        currentValue = currentScreen.textContent
+        console.log(currentValue)
+    }
+
+    // console.log(`${e.target.textContent} has been clicked`)
+    operator = e.target.textContent
     previousValue = currentScreen.textContent
     previousScreen.textContent = previousValue
     currentScreen.textContent = 0
-    
+    if (operator == '/'){
+        // divide(previousValue,currentValue)
+        console.log('this works')
+        desiredOperator = operator
+    }
+
+    else if (operator == 'x'){
+        // divide(previousValue,currentValue)
+        console.log('multiply opp')
+        desiredOperator = operator
+    }
+
+    else if (operator == '-'){
+        // divide(previousValue,currentValue)
+        console.log('subtract opp')
+        desiredOperator = operator
+    }
+
+    else if (operator == '+'){
+        // divide(previousValue,currentValue)
+        console.log('addition opp')
+        desiredOperator = operator
+    }
+
 }))
+
+
+// create evaluation function
+function evaluate(previousValue,currentValue){
+    if (desiredOperator == '/'){
+        divide(previousValue,currentValue)
+        console.log('this works now!')
+    }
+
+    else if(desiredOperator == 'x'){
+        multiply(previousValue,currentValue)
+    }
+
+    else if(desiredOperator == '-'){
+        subtract(previousValue, currentValue)
+    }
+
+    else if (desiredOperator == '+'){
+        add(previousValue,currentValue)
+    }
+}
+
+
+// create listener for equals button
+equals.addEventListener('click', function(){
+    previousValue = Math.floor(previousScreen.textContent)
+    currentValue = Math.floor(currentScreen.textContent)
+    console.log(previousValue)
+    console.log(currentValue)
+    evaluate(previousValue,currentValue)
+    
+
+})
+
+// need to address if user keeps clicking operators and never 
+// hits the equals sign b/w calculations
